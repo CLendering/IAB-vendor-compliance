3304187954a88192ce37da1847ab54eb879307f5
CLendering <82156467+CLendering@users.noreply.github.com>
2023-06-19 19:42:44 +0200

    Update reference-gvl.go
    - Added comments
    - Seperated functions

--- vendor-compliance-check/cross-reference-gvl/reference-gvl.go
+++ vendor-compliance-check/cross-reference-gvl/reference-gvl.go
@@ -6,151 +6,176 @@ import (
 	"strings"
 )
 
+// Define constants for file names
+const (
+	CookiesCSV          = "deny_all_vendors.csv"
+	GvlCSV              = "gvl_data.csv"
+	MatchedResultsCSV   = "matched_results.csv"
+	UnmatchedResultsCSV = "unmatched_results.csv"
+	PartialMatchCSV     = "partial_match_results.csv"
+)
+
 func main() {
-	// Read cookies CSV
-	cookiesFile, err := os.Open("deny_all_vendors.csv")
-	if err != nil {
-		panic(err)
-	}
-	defer cookiesFile.Close()
+	cookies := readCSV(CookiesCSV)
+	vendors := readCSV(GvlCSV)
 
-	cookiesReader := csv.NewReader(cookiesFile)
-	cookies, err := cookiesReader.ReadAll()
-	if err != nil {
-		panic(err)
-	}
+	matchedWriter := createCSVWriter(MatchedResultsCSV)
+	defer matchedWriter.Flush()
 
-	// Read vendors CSV
-	vendorsFile, err := os.Open("gvl_data.csv")
-	if err != nil {
-		panic(err)
-	}
-	defer vendorsFile.Close()
+	unmatchedWriter := createCSVWriter(UnmatchedResultsCSV)
+	defer unmatchedWriter.Flush()
 
-	vendorsReader := csv.NewReader(vendorsFile)
-	vendors, err := vendorsReader.ReadAll()
-	if err != nil {
-		panic(err)
-	}
-
-	// Create results CSV for matched cookies
-	matchedFile, err := os.Create("matched_results.csv")
-	if err != nil {
-		panic(err)
-	}
-	defer matchedFile.Close()
-
-	matchedWriter := csv.NewWriter(matchedFile)
-
-	// Create results CSV for unmatched cookies
-	unmatchedFile, err := os.Create("unmatched_results.csv")
-	if err != nil {
-		panic(err)
-	}
-	defer unmatchedFile.Close()
-
-	unmatchedWriter := csv.NewWriter(unmatchedFile)
-
-	// Create results CSV for partially matched cookies
-	partialMatchFile, err := os.Create("partial_match_results.csv")
-	if err != nil {
-		panic(err)
-	}
-	defer partialMatchFile.Close()
-
-	partialMatchWriter := csv.NewWriter(partialMatchFile)
+	partialMatchWriter := createCSVWriter(PartialMatchCSV)
+	defer partialMatchWriter.Flush()
 
 	// Iterate through cookies
 	for _, cookie := range cookies {
-		cookieDomain := strings.ReplaceAll(cookie[1], " ", "")
-		cookieName := strings.ReplaceAll(cookie[2], " ", "")
-		foundMatch := false
-		partialMatch := false
+		processCookie(cookie, vendors, matchedWriter, unmatchedWriter, partialMatchWriter)
+	}
+}
 
-		var partialMatchVendor []string
+// readCSV reads a CSV file and returns its content.
+func readCSV(filename string) [][]string {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
 
-		// Iterate through vendors
-		for _, vendor := range vendors {
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+	return records
+}
 
-			vendorCookieDomains := strings.Split(strings.ReplaceAll(vendor[4], " ", ""), ";")
-			vendorGeneralDomains := strings.Split(strings.ReplaceAll(vendor[7], " ", ""), ";")
-			vendorDomains := append(vendorCookieDomains, vendorGeneralDomains...)
-			vendorCookies := strings.Split(strings.ReplaceAll(vendor[5], " ", ""), ";")
-			cookiePurposes := strings.Split(vendor[6], ";")
+// createCSVWriter creates and returns a CSV writer
+func createCSVWriter(filename string) *csv.Writer {
+	file, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
 
-			domainMatched := false
+	return csv.NewWriter(file)
+}
 
-			// Iterate through vendor domains
-			for _, vendorDomain := range vendorDomains {
-				if domainMatches(cookieDomain, vendorDomain) {
-					domainMatched = true
+// processCookie processes a single cookie by checking it against vendors and writing match results.
+func processCookie(cookie []string, vendors [][]string, matchedWriter, unmatchedWriter, partialMatchWriter *csv.Writer) {
+	cookieDomain := strings.ReplaceAll(cookie[1], " ", "")
+	cookieName := strings.ReplaceAll(cookie[2], " ", "")
+	foundMatch := false
+	partialMatch := false
+	var partialMatchVendor []string
+
+	// Iterate through vendors
+	for _, vendor := range vendors {
+		vendorDomains, vendorCookies := extractVendorData(vendor)
+
+		if domainMatched := findDomainMatch(cookieDomain, vendorDomains); domainMatched {
+			partialMatch = true
+			partialMatchVendor = vendor
+			for i, vendorCookie := range vendorCookies {
+				if cookieName == vendorCookie {
+					foundMatch = true
+					writeMatchResult(matchedWriter, cookie, vendor, cookieName, cookieDomain, i)
 					break
 				}
 			}
-
-			if domainMatched {
-				partialMatch = true
-				partialMatchVendor = vendor
-				// Iterate through vendor cookies
-				for i, vendorCookie := range vendorCookies {
-					if cookieName == vendorCookie {
-						foundMatch = true
-						// Write matched result
-						row := []string{cookie[0], vendor[0], vendor[1], vendor[2], cookieName, cookieDomain, cookiePurposes[i]}
-						err = matchedWriter.Write(row)
-						if err != nil {
-							panic(err)
-						}
-						break
-					}
-				}
-
-			}
-
-			if foundMatch {
-				break
-			}
 		}
 
-		if !foundMatch {
-
-			if partialMatch {
-				// Write partially matched cookie to partial_match_results.csv
-				row := []string{cookie[0], partialMatchVendor[0], partialMatchVendor[1], partialMatchVendor[2], cookieName, cookieDomain}
-				err = partialMatchWriter.Write(row)
-				if err != nil {
-					panic(err)
-				}
-			} else {
-				// Write unmatched cookie to unmatched_results.csv
-				row := []string{cookie[0], cookieName, cookieDomain}
-				err = unmatchedWriter.Write(row)
-				if err != nil {
-					panic(err)
-				}
-			}
+		if foundMatch {
+			break
 		}
 	}
 
-	matchedWriter.Flush()
-	unmatchedWriter.Flush()
-	partialMatchWriter.Flush()
+	writePartialOrUnmatchedResult(partialMatch, foundMatch, partialMatchWriter, unmatchedWriter, cookie, partialMatchVendor, cookieName, cookieDomain)
 }
 
-func domainMatches(cookieDomain, vendorDomain string) bool {
+// extractVendorData extracts vendor data from a row in the GVL data.
+func extractVendorData(vendor []string) ([]string, []string) {
+	vendorCookieDomains := strings.Split(strings.ReplaceAll(vendor[4], " ", ""), ";")
+	vendorGeneralDomains := strings.Split(strings.ReplaceAll(vendor[7], " ", ""), ";")
+	vendorDomains := append(vendorCookieDomains, vendorGeneralDomains...)
+	vendorCookies := strings.Split(strings.ReplaceAll(vendor[5], " ", ""), ";")
+	return vendorDomains, vendorCookies
+}
 
+// findDomainMatch checks if a cookie's domain matches a vendor's domains.
+func findDomainMatch(cookieDomain string, vendorDomains []string) bool {
+	for _, vendorDomain := range vendorDomains {
+		if domainMatches(cookieDomain, vendorDomain) {
+			return true
+		}
+	}
+	return false
+}
+
+// writeMatchResult writes a match result to the matchedWriter.
+func writeMatchResult(matchedWriter *csv.Writer, cookie, vendor []string, cookieName, cookieDomain string, i int) {
+	row := []string{cookie[0], vendor[0], vendor[1], vendor[2], cookieName, cookieDomain, vendor[6]}
+	err := matchedWriter.Write(row)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// writePartialOrUnmatchedResult writes the results to the appropriate writer based on the match status.
+func writePartialOrUnmatchedResult(partialMatch, foundMatch bool, partialMatchWriter, unmatchedWriter *csv.Writer, cookie, partialMatchVendor []string, cookieName, cookieDomain string) {
+	if !foundMatch {
+		if partialMatch {
+			writePartialMatchResult(partialMatchWriter, cookie, partialMatchVendor, cookieName, cookieDomain)
+		} else {
+			writeUnmatchedResult(unmatchedWriter, cookie, cookieName, cookieDomain)
+		}
+	}
+}
+
+// writePartialMatchResult writes a partial match result to the partialMatchWriter.
+func writePartialMatchResult(partialMatchWriter *csv.Writer, cookie, partialMatchVendor []string, cookieName, cookieDomain string) {
+	row := []string{cookie[0], partialMatchVendor[0], partialMatchVendor[1], partialMatchVendor[2], cookieName, cookieDomain}
+	err := partialMatchWriter.Write(row)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// writeUnmatchedResult writes an unmatched result to the unmatchedWriter.
+func writeUnmatchedResult(unmatchedWriter *csv.Writer, cookie []string, cookieName, cookieDomain string) {
+	row := []string{cookie[0], cookieName, cookieDomain}
+	err := unmatchedWriter.Write(row)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// domainMatches checks if the cookie domain matches the vendor domain.
+func domainMatches(cookieDomain, vendorDomain string) bool {
+	// Split both domains into segments
 	vendorSplit := strings.Split(vendorDomain, ".")
 	cookieSplit := strings.Split(cookieDomain, ".")
-	matchCount := 0
 
-	for _, v_seg := range vendorSplit {
-		for _, c_seg := range cookieSplit {
-			if v_seg == c_seg {
-				matchCount++
-			}
+	// Determine which domain is less specific
+	var smallerDomain, largerDomain []string
+	if len(cookieSplit) < len(vendorSplit) {
+		smallerDomain, largerDomain = cookieSplit, vendorSplit
+	} else {
+		smallerDomain, largerDomain = vendorSplit, cookieSplit
+	}
+
+	// Match from right to left (from TLD to subdomain)
+	for i := 0; i < len(smallerDomain); i++ {
+		// Get the corresponding segments from the end of both split arrays
+		smallerSegment := smallerDomain[len(smallerDomain)-1-i]
+		largerSegment := largerDomain[len(largerDomain)-1-i]
+
+		// If the segments don't match, return false
+		if smallerSegment != largerSegment {
+			return false
 		}
 	}
 
-	return matchCount > 1
-
+	// If all segments matched, return true
+	return true
 }
